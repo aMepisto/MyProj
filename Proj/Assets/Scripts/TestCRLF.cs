d5a93dd09a7a68e1a4379a02e0c148027b11f2bb
aMepisto <dotorial83@gmail.com>
2015-10-20 18:04:15 +0900

    Proj Base work

---
+++ Proj/Assets/Scripts/TestCRLF.cs
@@ -0,0 +1,18 @@
+﻿// UTF8 & LF (유티에프:)
+
+using UnityEngine;
+using System.Collections;
+
+public class TestCRLF : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{	
+	}
+}
