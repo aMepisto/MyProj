cccfeef6a1be4b682b75c0573f0af0ba0a6fce27
aMepisto <dotorial83@gmail.com>
2016-03-15 16:56:26 +0900

    ShakeDevice

---
+++ Proj/Assets/Scripts/MeasureShaking.cs
@@ -0,0 +1,131 @@
+﻿//#define MEPI_DEV1
+#define MEPI_DEV2
+
+using UnityEngine;
+using System.Collections;
+
+public class MeasureShaking : MonoBehaviour
+{
+#if MEPI_DEV1
+    public float avrgTime = 0.5f;
+    public float peakLevel = 0.6f;
+    public float endCountTime = 0.6f;
+    public int shakeDir;
+    public int shakeCount;
+
+    Vector3 avrgAcc = Vector3.zero;
+    int countPos;
+    int countNeg;
+    int lastPeak;
+    int firstPeak;
+    bool counting;
+    float timer;
+#elif MEPI_DEV2
+
+    // TODO : Catch Device Shake.
+    private float accelerometerUpdateInterval = 1.0f / 60.0f;
+    // The greater the value of LowPassKernelWidthInSeconds, the slower the filtered value will converge towards current input sample (and vice versa).
+    private float lowPassKernelWidthInSeconds = 1.0f;
+    // This next parameter is initialized to 2.0 per Apple's recommendation, or at least according to Brady! ;)
+    private float shakeDetectionThreshold = 2.0f;
+
+    private float lowPassFilterFactor;
+    private Vector3 lowPassValue = Vector3.zero;
+    private Vector3 acceleration;
+    private Vector3 deltaAcceleration;
+#endif    
+
+#if MEPI_DEV1
+    bool ShakeDetector()
+    {
+        // read acceleration:
+        Vector3 curAcc = Input.acceleration;
+        // update average value:
+        avrgAcc = Vector3.Lerp(avrgAcc, curAcc, avrgTime * Time.deltaTime);
+        // calculate peak size:
+        curAcc -= avrgAcc;
+        // variable peak is zero when no peak detected...
+        int peak = 0;
+        // or +/- 1 according to the peak polarity:
+        if (curAcc.y > peakLevel) peak = 1;
+        if (curAcc.y < -peakLevel) peak = -1;
+        // do nothing if peak is the same of previous frame:
+        if (peak == lastPeak)
+            return false;
+        // peak changed state: process it
+        lastPeak = peak; // update lastPeak
+        if (peak != 0)
+        { // if a peak was detected...
+            timer = 0; // clear end count timer...
+            if (peak > 0) // and increment corresponding count
+                countPos++;
+            else
+                countNeg++;
+            if (!counting)
+            { // if it's the first peak...
+                counting = true; // start shake counting
+                firstPeak = peak; // save the first peak direction
+            }
+        }
+        else // but if no peak detected...
+            if (counting)
+            { // and it was counting...
+                timer += Time.deltaTime; // increment timer
+                if (timer > endCountTime)
+                { // if endCountTime reached...
+                    counting = false; // finish counting...
+                    shakeDir = firstPeak; // inform direction of first shake...
+                    if (countPos > countNeg) // and return the higher count
+                        shakeCount = countPos;
+                    else
+                        shakeCount = countNeg;
+                    // zero counters and become ready for next shake count
+                    countPos = 0;
+                    countNeg = 0;
+                    return true; // count finished
+                }
+            }
+        return false;
+    }
+#elif MEPI_DEV2
+    void Start()
+    {
+        lowPassFilterFactor = accelerometerUpdateInterval / lowPassKernelWidthInSeconds;
+        shakeDetectionThreshold *= shakeDetectionThreshold;
+        lowPassValue = Input.acceleration;
+    }
+#endif
+
+    void Update()
+    {
+        
+#if MEPI_DEV1
+        if (ShakeDetector())
+        { // call ShakeDetector every Update!
+            // the device was shaken up and the count is in shakeCount
+            // the direction of the first shake is in shakeDir (1 or -1)
+        }
+        // the variable counting tells when the device is being shaken:
+        if (counting)
+        {
+            //print("Shaking up device");
+        }
+#elif MEPI_DEV2
+        acceleration = Input.acceleration;
+        lowPassValue = Vector3.Lerp(lowPassValue, acceleration, lowPassFilterFactor);
+        deltaAcceleration = acceleration - lowPassValue;
+        if (deltaAcceleration.sqrMagnitude >= shakeDetectionThreshold)
+        {
+            // Perform your "shaking actions" here, with suitable guards in the if check above, if necessary to not, to not fire again if they're already being performed.
+            //Debug.LogError("Shake event detected at time " + Time.time);
+            Handheld.Vibrate();
+        }
+#endif
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 10, 100, 32), "Vibrate!"))
+            Handheld.Vibrate();
+    }
+}
---
+++ Proj/Assets/Scripts/TestCircle.cs
@@ -0,0 +1,251 @@
+﻿// UTF8 & LF (유티에프:)
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TestCircle : MonoBehaviour
+{
+    #region variables
+    private readonly string SHADER_NAME = "Particles/Additive";
+    private readonly int CIRCLE_RADIUS = 200;
+    private readonly int POINT_COUNT = 50;
+    private readonly float ROTATE_ANGLE = 5f;
+    private float _circleRadius;
+    private float _speed;
+    private bool _isUpdate;
+    private bool _reverse;
+    private float _rotateAngle;
+    private bool _isUseLineRenderer;
+    RectTransform _rectTransform;
+
+    // note : Use LineRenderer
+    private int _lineSegments;
+    private float _lineStartWidth;
+    private float _lineEndWidth;
+    private float _lineAngle;
+    LineRenderer _lineRenderer;
+
+    // note : Use MeshRenderer
+    Quaternion _meshQuaternion = Quaternion.identity;
+    private int _meshNumOfPoints;
+    private Mesh _circleMesh = null;
+    public List<Vector3> _meshVertexList = new List<Vector3>();
+    private List<int> _meshTriangleList = new List<int>();
+    private MeshRenderer _circleMeshRenderer = new MeshRenderer();
+    private MeshFilter _circlaMeshFilter = new MeshFilter();
+    #endregion variables
+
+    private void Awake()
+    {
+        this.gameObject.layer = LayerMask.NameToLayer("UI");
+
+        Refresh();
+    }
+
+    private void Refresh()
+    {
+        this._lineSegments = POINT_COUNT;
+        this._lineStartWidth = 0.1f;
+        this._lineEndWidth = 0.1f;
+        this._meshNumOfPoints = POINT_COUNT;
+        this._rotateAngle = ROTATE_ANGLE;
+        this._isUpdate = false;
+        this._reverse = false;
+        this._isUseLineRenderer = false;
+        this._circleRadius = CIRCLE_RADIUS;
+        this._speed = 100;
+    }
+
+    private void Start()
+    {
+        if (_rectTransform == null)
+            _rectTransform = this.GetComponent<RectTransform>();
+
+        InitLineRenderer();
+        InitMeshRenderer();
+    }
+
+    private void InitLineRenderer()
+    {
+        _lineRenderer = gameObject.GetComponent<LineRenderer>();
+        if (_lineRenderer != null)
+        {
+            _lineRenderer.material = new Material(Shader.Find(SHADER_NAME));
+            _lineRenderer.SetVertexCount(_lineSegments + 1);
+            _lineRenderer.SetWidth(_lineStartWidth, _lineEndWidth);
+            _lineRenderer.useWorldSpace = false;
+        }
+    }
+
+    private void InitMeshRenderer()
+    {
+        float angleStep = 360.0f / (float)_meshNumOfPoints;
+        _meshQuaternion = Quaternion.Euler(0.0f, 0.0f, angleStep);
+        _circleMeshRenderer = this.gameObject.GetComponent<MeshRenderer>();
+        if (_circleMeshRenderer == null)
+            _circleMeshRenderer = this.gameObject.AddComponent<MeshRenderer>();
+        _circleMeshRenderer.material = new Material(Shader.Find(SHADER_NAME));
+
+        _circlaMeshFilter = this.gameObject.GetComponent<MeshFilter>();
+        if (_circlaMeshFilter == null)
+            _circlaMeshFilter = this.gameObject.AddComponent<MeshFilter>();
+
+        CreateMeshCircle();
+    }
+
+    private void Update()
+    {
+        if (_isUpdate == false)
+            return;
+
+        if (_isUseLineRenderer)
+            UpdateLineRendererPoints();
+        else
+            UpdateMeshRendererPoints();
+
+        if (_isUpdate)
+        {
+            _rectTransform.Rotate(Vector3.forward, _rotateAngle);
+            _circleRadius -= Time.deltaTime * _speed;
+            if (_reverse)
+            {
+                _reverse = false;
+                _speed *= -1f;
+                if (_circleRadius < 0)
+                {
+                    _circleRadius = CIRCLE_RADIUS * -1f;
+                }
+                else if (0 < _circleRadius)
+                {
+                    _circleRadius = CIRCLE_RADIUS;
+                }
+            }
+            else
+            {
+                if (CIRCLE_RADIUS <= Mathf.Abs(_circleRadius))
+                {
+                    _rotateAngle *= -1f;
+                    _reverse = true;
+                }
+            }
+        }
+    }
+
+    #region Use MeshRenderer
+    private void CreateMeshCircle()
+    {
+        MakeFirstTriangle();
+        AddTriandleIndices();
+
+        for (int i = 0; i < _meshNumOfPoints - 1; i++)
+        {
+            _meshTriangleList.Add(0);
+            _meshTriangleList.Add(_meshVertexList.Count - 1);
+            _meshTriangleList.Add(_meshVertexList.Count);
+            _meshVertexList.Add(_meshQuaternion * _meshVertexList[_meshVertexList.Count - 1]);
+        }
+    }
+
+    private void UpdateMeshRendererPoints()
+    {
+        ReSetFirstTriangle();
+        for (int i = 1; i < _meshNumOfPoints; i++)
+        {
+            _meshVertexList[i + 2] = _meshQuaternion * _meshVertexList[i + 1];
+        }
+
+        if (_circleMesh == null)
+            _circleMesh = new Mesh();
+        _circleMesh.vertices = _meshVertexList.ToArray();
+        _circleMesh.triangles = _meshTriangleList.ToArray();
+
+        //Debug2.Assert(_circlaMeshFilter != null);
+        _circlaMeshFilter.mesh = _circleMesh;
+        _circlaMeshFilter.name = "testMeshCircle";
+    }
+
+    private void MakeFirstTriangle()
+    {
+        // Make first triangle.
+        //Debug2.Assert(_meshVertexList != null);
+        _meshVertexList.Add(new Vector3(0.0f, 0.0f, -1000f));           // 1. Circle center.
+        _meshVertexList.Add(new Vector3(0.0f, _circleRadius, 0.0f));    // 2. First vertex on circle outline (_circleRadius = 0.5f)
+        _meshVertexList.Add(_meshQuaternion * _meshVertexList[1]);      // 3. First vertex on circle outline rotated by angle)
+    }
+
+    private void ReSetFirstTriangle()
+    {
+        // Reset first triangle.
+        //Debug2.Assert(_meshVertexList != null);
+        _meshVertexList[0] = new Vector3(0.0f, 0.0f, -1000f);           // 1. Circle center.
+        _meshVertexList[1] = new Vector3(0.0f, _circleRadius, -1000f);  // 2. First vertex on circle outline (_circleRadius = 0.5f)
+        _meshVertexList[2] = _meshQuaternion * _meshVertexList[1];      // 3. First vertex on circle outline rotated by angle)
+    }
+
+    private void AddTriandleIndices()
+    {
+        // Add triangle indices.
+        //Debug2.Assert(_meshTriangleList != null);
+        _meshTriangleList.Add(0);
+        _meshTriangleList.Add(1);
+        _meshTriangleList.Add(2);
+    }
+    #endregion Use MeshRenderer
+
+    #region Use LineRenderer
+    private void UpdateLineRendererPoints()
+    {
+        if (_circleRadius < 0)
+            _lineAngle = -20f;
+        else if (0 < _circleRadius)
+            _lineAngle = 20f;
+
+        float x;
+        float y;
+        float z = -1000f;
+
+        for (int i = 0; i < (_lineSegments + 1); i++)
+        {
+            x = Mathf.Sin(Mathf.Deg2Rad * _lineAngle) * _circleRadius;
+            y = Mathf.Cos(Mathf.Deg2Rad * _lineAngle) * _circleRadius;
+
+            _lineRenderer.SetPosition(i, new Vector3(x, y, z));
+            _lineAngle += (360f / _lineSegments);
+        }
+    }
+    #endregion Use LineRenderer
+
+#if UNITY_EDITOR
+    private void OnGUI()
+    {
+        if (GUILayout.Button(_isUseLineRenderer == true ? "use mesh" : "use line"))
+        {
+            _lineRenderer.enabled = false;
+            _circleMeshRenderer.enabled = false;
+
+            _isUseLineRenderer = !_isUseLineRenderer;
+            if (_isUseLineRenderer)
+                _lineRenderer.enabled = true;
+            else
+                _circleMeshRenderer.enabled = true;
+        }
+
+        if (GUILayout.Button(_isUpdate == true ? "Stop" : "Play"))
+        {
+            _isUpdate = !_isUpdate;
+            if (_isUpdate == false)
+                Debug.LogError("CircleRadius : " + _circleRadius);
+        }
+
+        if (GUILayout.Button("ScreenSize"))
+        {
+            Resolution[] resolutions = Screen.resolutions;
+            foreach (Resolution res in resolutions)
+            {
+                Debug.LogError(res.width + "x" + res.height);
+            }
+        }
+    }
+#endif
+}
